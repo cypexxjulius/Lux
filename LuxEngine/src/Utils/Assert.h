315ef2d8cd28a66907dad36c2f858174a7a7a8cb
juliushaag <haag.julius@outlook.de>
2022-08-22 17:32:12 +0200

    Implemented a linux version for our debug functions

--- LuxEngine/src/Utils/Assert.h
+++ LuxEngine/src/Utils/Assert.h
@@ -1,6 +1,14 @@
 #pragma once 
 #include <assert.h>
 
+#ifdef LX_WIN
 #define Verify(boolean) if(!(boolean)) __debugbreak();
 
 #define TODO() __debugbreak();
+
+#elif LX_UNIX
+#include <signal>
+#define Verify(boolean) if(!(boolean)) raise(SIGTRAP);
+
+#define TODO() raise(SIGTRAP);
+#endif 
