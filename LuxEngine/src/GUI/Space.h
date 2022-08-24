cc1271623e1b1c140e1f8fa2e8d650ec877d5ca1
juliushaag <haag.julius@outlook.de>
2022-08-24 19:05:49 +0200

    Added a seperate gui space class for handling space transition

---
+++ LuxEngine/src/GUI/Space.h
@@ -0,0 +1,46 @@
+#pragma once
+#pragma once
+
+namespace Lux::GUI
+{
+    class GUISpace
+    {
+    public:
+
+        static constexpr v3 ToRenderSpaceOrigin(float aspect_ratio)
+        {
+            return { -aspect_ratio, -1.0f, 0.0f };
+        }
+
+        static inline v3 ToRenderSpace(float aspect_ratio, const v3& position)
+        {
+            return { (position.x * 2.0f * aspect_ratio) - aspect_ratio, (position.y * 2.0f - 1.0f), position.z * 0.5f };
+        }
+
+        static inline v3 ToRenderSpaceDelta(float aspect_ratio, v3 position)
+        {
+            return { position.x * 2.0f * aspect_ratio, position.y * 2.0f, position.z };
+        }
+
+        static inline float ToRenderSpaceX(float aspect_ratio, float x)
+        {
+            return (x * 2.0f * aspect_ratio);
+        }
+
+        static inline float ToRenderSpaceY(float y)
+        {
+            return (y * 2.0f);
+        }
+
+        static inline float FromRenderSpaceX(float aspect_ratio, float x)
+        {
+            return (x / (2.0f * aspect_ratio));
+        }
+        static inline float FromRenderSpaceY(float y)
+        {
+            return (y / 2.0f);
+        }
+
+    };
+
+}
